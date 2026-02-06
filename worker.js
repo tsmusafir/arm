bbb7bdb8a0aa017a4d2dc5ea58d957a30806bbc7
tsmusafir <talhaalisaleem@gmail.com>
2026-02-06 15:53:57 +0500

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,9 @@
+{
+  "redirect_workers": [
+    "https://arm.itsawaisone.workers.dev",
+    "https://arm1.itsawaisone.workers.dev",
+    "https://arm2.itsawaisone.workers.dev",
+    "https://arm3.itsawaisone.workers.dev",
+    "https://arm4.itsawaisone.workers.dev"
+  ]
+}
